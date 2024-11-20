1237dfdabda11640ad134b85192c960c3b78aa6b
Luis Busta <lfbusta@icloud.com>
2024-11-20 17:32:33 +0100

    Remove html-minifier.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,8 @@
 # Create the build folder if it doesn't exist
 mkdir -p docs
 
-# Minify the HTML and JS
-# NOTE: This command for some reason still grabs tailwind's css files. Even though it shouldn't.
-npx html-minifier --collapse-whitespace --remove-comments --input-dir ./src --output-dir ./docs
+# Move index.html file to docs.
+cp ./src/index.html ./docs/index.html
 
 # Compile the Tailwind CSS and minify it
 npx tailwindcss -i ./src/styles.css -o ./docs/styles.css --minify
